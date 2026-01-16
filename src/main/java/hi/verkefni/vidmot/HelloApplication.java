d7baade6b0f2ba9374874b24023427130c47579e
valdis-anna <vav22@hi.is>
2026-01-16 22:31:16 +0000

    Bætti við pakka og tveimur klösum

---
+++ src/main/java/hi/verkefni/vidmot/HelloApplication.java
@@ -0,0 +1,7 @@
+package hi.verkefni.vidmot;
+
+public class HelloApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/hi/verkefni/vidmot/HelloController.java
@@ -0,0 +1,7 @@
+package hi.verkefni.vidmot;
+
+public class HelloController {
+    public static void main(String[] args) {
+
+    }
+}
